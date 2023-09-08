4087f86b5695776b91391caaf0573fca9d81b7d8
Vishal Singh <91767329+vishalsingh1997@users.noreply.github.com>
2023-09-08 12:20:25 +0000

    data type conversion

---
+++ basics/03_datatypes.js
@@ -0,0 +1,27 @@
+// Types of data in javaScript 
+// Primitive data Types
+// Number
+// String
+// Boolean
+// BigInt
+// undefined
+// null
+
+// Non-Primitive Data Type 
+// Object
+// Array
+
+
+let data = 22.2;
+
+let a = "hello"
+
+// console.log(typeof data);
+
+let score = "99"
+// Now converting it into Number 
+
+let p = Number(score)  //data type conversion string => Number
+let b = BigInt(score)  //data type conversion string => BigInt
+let c = String(p) //data type conversion Number => String
+console.log(typeof b);
