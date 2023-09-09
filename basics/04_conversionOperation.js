96c0c958208d326ea21d2cae9d6886b62a8f82a8
Vishal Singh <91767329+vishalsingh1997@users.noreply.github.com>
2023-09-09 11:35:59 +0000

    conversionOperation

---
+++ basics/04_conversionOperation.js
@@ -0,0 +1,18 @@
+// #############.......Operations.........###########
+
+let value = 2;
+let negativeValue = -value;
+// console.log(negativeValue);
+
+let x = 3;
+console.log(x); //Output: 3
+const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
